92e02b29686c69c1c6a7e19b0ac0ac5651b9199e
LandfillHeart <kimbleredward@gmail.com>
2025-10-17 12:52:52 +0200

    Inizio IObserver
    - Inizio stesura degli observer, partendo dagli observer che avvisano cambiamenti al sistema di pricing

---
+++ ProvaTecnica/Observer/IObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaTecnica.Observer
+{
+	// when the pricing context changes, the user has to be notified
+	// this allows us to send notifications such as new sales happening
+	public interface IPricingContextObserver
+	{
+		public void ContextChanged();
+	}
+
+	public interface IPricingContextObservable
+	{
+		public void AddObserver(IPricingContextObserver observer); 
+		public void RemoveObserver(IPricingContextObserver observer);
+		public void NotifyObservers();
+	}
+}
